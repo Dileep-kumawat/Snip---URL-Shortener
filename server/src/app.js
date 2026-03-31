7477e4e223ad819538d6965637560912561c9be7
Dileep <dileepkumawat525@gmail.com>
2026-03-31 17:58:55 +0530

    Backend completed.

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const urlRouter = require("../routes/url.route");
 
 const app = express();
+
+app.use(express.json());
+
+/**
+ * @route /
+ * @description all routes goes here
+ */
+app.use("/", urlRouter);
 
 module.exports = app;
